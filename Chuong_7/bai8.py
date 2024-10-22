eb6896ce641f3d9e0a90b66fba57e3aad23c8da7
Tran Thi Thuy Linh <tttlinh.24174600088@sv.uneti.edu.vn>
2024-10-22 19:33:58 +0700

    Chuong_7

--- Chuong_7/bai8.py
+++ Chuong_7/bai8.py
@@ -1,7 +1,7 @@
 import math 
-x = float(input('Nhập giá trị của x'))
-if 0 < x < 1 and x > 1:
-    F = math.log(4,x) + math.log(x, 2)
+x = float(input('Nhập giá trị của x:'))
+if 0 < x < 1 or x > 1:
+    F = math.log(x, 4) + math.log(2, x)
     print(f'Gia tri cua bieu thuc la: {F:.2f}')
 else:
     print('Khong tinh duoc gia tri F')    
