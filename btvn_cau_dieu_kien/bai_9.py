cc1f567b8631455352dcd7f720f7f7c7ba7f3846
Tran Thi Thuy Linh <tttlinh.24174600088@sv.uneti.edu.vn>
2024-10-31 09:07:38 +0700

    bai_9

--- btvn_cau_dieu_kien/bai_9.py
+++ btvn_cau_dieu_kien/bai_9.py
@@ -1,6 +1,6 @@
 # Tính cước tacxi: 
 print("1. Xe 4 chỗ\n2. Xe 7 chỗ")
-loaixe = float(input("Nhập loại xe bạn muốn tính:"))
+loaixe = float(input("Nhập loại xe bạn muốn đi:"))
 if loaixe != 1 and loaixe != 2:
     print("Bạn chỉ được chọn 1 trong 2 xe")
 # -	Loại xe 4 chỗ
@@ -13,14 +13,14 @@ elif loaixe == 1:
         #đi dưới 20km thì trừ đi 0.8km giá mở cửa
         gia_mua_cua = 11000
         km_dichuyen = ((km - 0.8)) * 12100
-        print(f"Số tiền phải bố thí là {float(gia_mua_cua + km_dichuyen)}vnđ")
+        print(f"Số tiền phải trả là {float(gia_mua_cua + km_dichuyen)}vnđ")
 
     elif km > 20:
         #đi hơn 20km thì trừ đi 0.8 với 20km đầu.
         gia_mua_cua = 11000
         km_20 = 20 * 12100
         km_dichuyen = (km - 0.8 - 20) * 10000
-        print(f"Số tiền phải bố thí là {float(gia_mua_cua + km_dichuyen)}vnđ")
+        print(f"Số tiền phải trả là {float(gia_mua_cua + km_dichuyen)}vnđ")
 
 
 
@@ -34,11 +34,11 @@ elif loaixe == 2:
         #đi dưới 30km thì trừ đi 0.8km giá mở cửa
         gia_mua_cua = 13000
         km_dichuyen = ((km - 0.8)) * 14100
-        print(f"Số tiền phải bố thí là {float(gia_mua_cua + km_dichuyen)}vnđ")
+        print(f"Số tiền phải trả là {float(gia_mua_cua + km_dichuyen)}vnđ")
 
     elif km > 30:
         #đi hơn 30km thì trừ đi 0.8 với 30km đầu.
         gia_mua_cua = 13000
         km_20 = 20 * 12100
         km_dichuyen = (km - 0.8 - 20) * 12000
-        print(f"Số tiền phải bố thí là {float(gia_mua_cua + km_dichuyen)}vnđ")
+        print(f"Số tiền phải trả là {float(gia_mua_cua + km_dichuyen)}vnđ")
